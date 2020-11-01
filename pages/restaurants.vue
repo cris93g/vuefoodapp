fd1a547a9d4ef36808f6a2732d2bae98b6ab1228
cris93g <37388115+cris93g@users.noreply.github.com>
2020-11-01 17:44:53 -0600

    learning

---
+++ pages/restaurants.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="container restaurant">
+        <h1>Restaurants</h1>
+        <AppRestaurantInfo/>
+    </main>
+</template>
+
+<script>
+    import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue"
+    export default {
+        AppRestaurantInfo,
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
