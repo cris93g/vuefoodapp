4519ea9972fe8c43ddd5f5eb550a65988676cca7
Gonzalez <crgonzalez@teksystems.com>
2020-11-02 09:21:40 -0600

    yea

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="masthead" role="img" aria-label="Image Description">
     <h1>
-      Nuxt Food App
+      Foodyapp
     </h1>
     <nuxt-link to="/restaurants">
       <button>
